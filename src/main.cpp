4dc1e72dd9f868b6878e1c6beb10d20399e4e21a
Pepe <pepe@gmail.com>
2021-12-21 13:33:49 +0100

    holiwi uwu

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int fin;
+
+    cout << "iiiuiiiuiiuiuiuiuiuiuiuiii" << endl;
+
+    cout << "Hola a todo el mundo" << endl;
+
+    cin >> fin;
+    
+    return 0;
+}
